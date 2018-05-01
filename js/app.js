1b190da8b34f16111849096eaf49567a71393422
Emily Qinglu Zhang <emilyqlzhang@gmail.com>
2018-05-01 11:21:44 -0400

    Implement Enemy.prototype's update function

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + 101 * dt;
+    console.log(this.x);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,7 +38,7 @@ Player.prototype.update = function() {
 
 };
 
-Player.prototype.update = function() {
+Player.prototype.render = function() {
 
 };
 
